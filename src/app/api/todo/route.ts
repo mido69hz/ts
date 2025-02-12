e0b342cb039f6dd4c19266844ff1327bb74e0360
24HP7578 24HP7578 <24HP7578@admins-iMac.local>
2025-02-12 15:33:46 +0800

    ts

---
+++ src/app/api/todo/route.ts
@@ -0,0 +1,18 @@
+import { Todo } from "@/types/Todo";
+import { nanoid } from "nanoid";
+import { NextRequest, NextResponse } from "next/server";
+
+export const todos: Todo[] = [
+  { id: nanoid(), title: "Sereerei zaluusai", checked: true },
+];
+
+export const GET = () => {
+  return NextResponse.json(todos);
+};
+
+export const POST = async (req: NextRequest) => {
+  const { title } = await req.json();
+  const newTodo = { id: nanoid(), title: title, checked: false };
+  todos.push(newTodo);
+  return NextResponse.json(newTodo);
+};
---
+++ test.sql
@@ -0,0 +1,4 @@
+select * from people where age <= 21;
+select * from people where age >= 21;
+select * from people where age >= 21 AND gender = 'female';
+select * from people where age >= 21 AND gender != 'female';
